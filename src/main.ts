a091dfd35db802c97670c2c26b6c30cd60da5c99
François-David Collin <Francois-David.Collin@umontpellier.fr>
2024-03-03 13:44:12 +0100

    link updated

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ new Engine({
   experience: Flower,
   info: {
     github: 'https://github.com/fradav/flower-scale-relativity',
-    link: 'https://fradav.perso.math.cnrs.fr/Matière solaire.html',
+    link: 'https://fradav.perso.math.cnrs.fr/posts/Matière solaire.html',
     description: 'Hommage à Laurent Nottale. Simulation de la morphogenèse des fleurs à partir de la théorie de la relativité d’échelle.',
     documentTitle: 'Des Fleurs pour Schrödinger, Hommage à Laurent Nottale',
     title: 'Des Fleurs pour Schrödinger',
