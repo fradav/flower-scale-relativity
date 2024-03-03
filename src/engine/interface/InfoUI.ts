9f8c5bfc94617504466a464788b6b4015cb32f33
François-David Collin <Francois-David.Collin@umontpellier.fr>
2024-03-03 13:25:27 +0100

    cosmetics

--- src/engine/interface/InfoUI.ts
+++ src/engine/interface/InfoUI.ts
@@ -1,8 +1,10 @@
 import './info.scss'
-import githubLogo from '../../../assets/github.png'
+import githubLogo from '../../../assets/github.svg'
+import linkLogo from '../../../assets/link-solid.svg'
 
 export type InfoConfig = {
   github?: string
+  link?: string
   description?: string
   title?: string
   documentTitle?: string
@@ -28,6 +30,13 @@ ${
     : ``
 }
 <div class="social-container">
+${
+  config.link
+    ? `<a href="${config.link}" class="social-button" target="_blank">
+    <img src="${linkLogo}" alt="link Logo to the description" />
+  </a>`
+    : ``
+}
 ${
   config.github
     ? `<a href="${config.github}" class="social-button" target="_blank">
--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,9 @@ new Engine({
   experience: Flower,
   info: {
     github: 'https://github.com/fradav/flower-scale-relativity',
-    description: 'Simulation de la morphogenèse des fleurs à partir de la théorie de la relativité d’échelle.',
+    link: 'https://fradav.perso.math.cnrs.fr/Matière solaire.html',
+    description: 'Hommage à Laurent Nottale. Simulation de la morphogenèse des fleurs à partir de la théorie de la relativité d’échelle.',
     documentTitle: 'Des Fleurs pour Schrödinger, Hommage à Laurent Nottale',
-    title: 'Des Fleurs pour Schrödinger, Hommage à Laurent Nottale',
+    title: 'Des Fleurs pour Schrödinger',
   }
 })
